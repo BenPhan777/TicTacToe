55848d09b1d3d76b90ce6d865c6ed5228ac551ca
benphan <benphan6320@gmail.com>
2020-06-10 18:37:46 -0600

    Started the "Tic Tac Toe" project, added author and project information.

---
+++ src/TicTacToe.java
@@ -0,0 +1,6 @@
+// Name: Ben Phan
+// Project Name: Tic Tac Toe
+// Date of Last Modification: June 10, 2020
+// Purpose: Display and allow users to play Tic Tac Toe through output
+public class TicTacToe {
+}
