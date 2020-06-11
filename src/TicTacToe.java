66930898af38b96ff1d4311deda6e7ddd5c3da8d
benphan <benphan6320@gmail.com>
2020-06-11 10:06:13 -0600

    Improved the documentation style and format.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,19 +1,20 @@
-//***************************************************************************
-//  Name: Ben Phan.
-//  Project Name: Tic Tac Toe.
-//  Date of Last Modification: June 10, 2020.
-//  Purpose: Display and allow users to play Tic Tac Toe through the console.
-//***************************************************************************
+/*****************************************************************************
+*   Name: Ben Phan.
+*   Project Name: Tic Tac Toe.
+*   Date of Last Modification: June 11, 2020.
+*   Purpose: Display and allow users to play Tic Tac Toe through the console.
+******************************************************************************/
 
 public class TicTacToe {
-    //*******************************************************************
-    // main(String[] args)
-    // Purpose: display a finished tic tac toe game board on the console.
-    // Pre-conditions:
-    //     - String[] args: a set of arguments, empty in this case.
-    // Post-conditions: display a tic tac toe game to the console.
-    // Return: None.
-    //*******************************************************************
+
+    /***********************************************************************
+    *   main(String[] args)
+    *   Purpose: display a finished tic tac toe game board on the console.
+    *   Pre-conditions:
+    *       @param args: an array of string arguments, empty in this case.
+    *   Post-conditions: display a tic tac toe game to the console.
+    *   Return: None.
+    ************************************************************************/
     public static void main(String[] args) {
         System.out.println("X O O");
         System.out.println("O X O");
