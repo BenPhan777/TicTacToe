bbee9ffa0a8589a90a0f3be0d02bdec4be7521cc
benphan <benphan6320@gmail.com>
2020-06-14 17:18:43 -0600

    Added toArray() and modified the script so that any size of the board game is possible. Also, tested toArray() and it passed all tests.

--- src/TicTacToeTest.java
+++ src/TicTacToeTest.java
@@ -8,14 +8,35 @@ class TicTacToeTest {
     void drawBoard() {
         TicTacToe test = new TicTacToe();
         // Board of size 3.
-        String[][] moves = {{"X", "_", "X"}, {"X", "O", "O"}, {"_", "O", "O"}};
+        char[][] moves = {{'X', '_', 'X'}, {'X', 'O', 'O'}, {'_', 'O', 'O'}};
         test.drawBoard(moves);
         // Board of size 4.
-        moves = new String[][]{{"X", "_", "X", "X"}, {"O", "_", "O", "O"}, {"X", "X", "_", "X"}, {"O", "O", "O", "O"}};
+        moves = new char[][]{{'X', '_', 'X', 'X'}, {'O', '_', 'O', 'O'}, {'X', 'X', '_', 'X'}, {'O', 'O', 'O', 'O'}};
         test.drawBoard(moves);
         // Board of size 5.
-        moves = new String[][]{{"X", "X", "_", "X", "X"}, {"O", "_", "O", "O", "O"},
-                                {"X", "_", "X", "X", "X"}, {"O", "O", "O", "O", "_"}, {"_", "O", "X", "O", "X"}};
+        moves = new char[][]{{'X', 'X', '_', 'X', 'X'}, {'O', '_', 'O', 'O', 'O'},
+                                {'X', '_', 'X', 'X', 'X'}, {'O', 'O', 'O', 'O', '_'}, {'_', 'O', 'X', 'O', 'X'}};
         test.drawBoard(moves);
+    }
+
+    @Test
+    void toArray() {
+        TicTacToe test = new TicTacToe();
+
+        // Board of size 3.
+        char[][] moves = {{'X', '_', 'X'}, {'X', 'O', 'O'}, {'_', 'O', 'O'}};
+        assertArrayEquals(test.toArray("X_XXOO_OO", 3), moves,
+                "Test to see if the given string gives the correct array of 2-dimensional array size 3");
+
+        // Board of size 4.
+        moves = new char[][]{{'X', '_', 'X', 'X'}, {'O', '_', 'O', 'O'}, {'X', 'X', '_', 'X'}, {'O', 'O', 'O', 'O'}};
+        assertArrayEquals(test.toArray("X_XXO_OOXX_XOOOO", 4), moves,
+                "Test to see if the given string gives the correct array of 2-dimensional array size 4");
+
+        // Board of size 5.
+        moves = new char[][]{{'X', 'X', '_', 'X', 'X'}, {'O', '_', 'O', 'O', 'O'},
+                {'X', '_', 'X', 'X', 'X'}, {'O', 'O', 'O', 'O', '_'}, {'_', 'O', 'X', 'O', 'X'}};
+        assertArrayEquals(test.toArray("XX_XXO_OOOX_XXXOOOO__OXOX", 5), moves,
+                "Test to see if the given string gives the correct array of 2-dimensional array size 5");
     }
 }
